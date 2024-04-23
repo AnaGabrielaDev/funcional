522d730a9ab6dbe7aae77ac8e0159d81a10e062c
Ana Gabriela <anagabrieladev@gmil.com>
2024-04-22 23:55:00 -0300

    chore: configs

--- src/index.py
+++ src/index.py
@@ -1,5 +1,5 @@
 # from q1_AnaBezerra import q1
-# q1()
+# q1("Credit", 100, True, "ana", "24110")
 
 # import q2_AnaBezerra 
 # q2_AnaBezerra.run_tests()
@@ -10,4 +10,4 @@
 # from q4_AnaBezerra import q4
 # q4()
 
-# from q5_AnaBezerra import create_account, fund_transfer,home
+from q5_AnaBezerra import create_account, fund_transfer,home
