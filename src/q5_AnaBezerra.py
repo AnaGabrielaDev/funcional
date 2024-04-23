bc7735cc2048d0a6763379ba49574438a774dc90
Ana Gabriela <anagabrieladev@gmil.com>
2024-04-22 23:55:00 -0300

    feat: q5

--- src/q5_AnaBezerra.py
+++ src/q5_AnaBezerra.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from q1_AnaBezerra import q1
 app = Flask(__name__)
 
 users_json = {}
 
-#QUESTÃO ESCOLHIDA: 1
+#QUESTÃO ESCOLHIDA: 1, o fluxo de transações foi implementado em um servidor web, onde é possível criar contas e realizar transferências de fundos
 #Somente para esclarecer que esses usuarios estao sendo criados in memory, nao persistem apos o server ser desligado
 
 @app.route('/create_account', methods=['POST'])
@@ -20,14 +20,25 @@ def create_account():
 def home():
     return "Hello, World!"
 
-@app.route('/fund_transfer', methods=['POST'])
-def fund_transfer():
-    account = request.form.get('user')
-    password = request.form.get('password')
-    if check_password_hash(users_json[account], password):
-        return "Fund transfer successful!"
-    else:
-        return "Invalid password!"
+get_account = lambda: request.form.get('user')
+get_password = lambda: request.form.get('password')
+get_amount = lambda: str(request.form.get('amount'))
+get_transaction_type = lambda: str(request.form.get('transaction_type'))
+
+transfer_message = lambda account, amount: "Transaction of " + amount + " by " + account + " successfully transferred!"
+invalid_password_message = lambda: "Invalid password!"
+check_password = lambda account, password: check_password_hash(users_json[account], password)
+
+@app.route('/payment', methods=['POST'])
+def payment():
+    transaction_type = get_transaction_type()
+    user = get_account()
+    password = get_password()
+    amount = get_amount()
+    allowed = check_password(user, password)
+    q1(transaction_type, amount, allowed, user, password) 
+    
+    return transfer_message(user, amount) if check_password(user, password) else invalid_password_message()
 
 if __name__ == '__main__':
     app.run(debug=True)
