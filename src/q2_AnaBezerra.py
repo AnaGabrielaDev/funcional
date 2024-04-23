22f61daddf55a43902f814acc0e40ee0844984ed
Ana Gabriela <anagabrieladev@gmil.com>
2024-04-22 23:55:00 -0300

    feat: q2

--- src/q2_AnaBezerra.py
+++ src/q2_AnaBezerra.py
@@ -1,20 +1,39 @@
-
 import unittest
+import q1_AnaBezerra as q1
+from unittest.mock import patch
+from io import StringIO
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+class TestModule(unittest.TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_receive_cash_input(self, mock_stdout):
+        q1.receive_cash_input(12)
+        self.assertEqual(mock_stdout.getvalue(), 12)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    @patch('builtins.input', return_value='100')
+    def test_receive_cash_input(self, input):
+        result = q1.receive_cash_input()
+        self.assertEqual(result, 100)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(3)
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_cancel_transaction(self, mock_stdout):
+        q1.cancel_transaction()
+        self.assertEqual(mock_stdout.getvalue(), 'Cancel Transaction\n')
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_close_transaction(self, mock_stdout):
+        q1.close_transaction()
+        self.assertEqual(mock_stdout.getvalue(), 'Close Transaction\n')
+
+stress_test_complete_transaction = lambda transaction_function, iterations: all(transaction_function() == 'Transaction completed!\n' for _ in range(iterations))
+
+complete_transaction = lambda :'Transaction completed!\n'
+
+run_tests = lambda: (
+    print('Running unit tests...'),
+    unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestModule)),
+    print('Running stress test...'),
+    [print('Stress test passed.') if stress_test_complete_transaction(complete_transaction, 10000) else print('Stress test failed.')]
+)
 
 if __name__ == '__main__':
-    unittest.main()
+    run_tests()
