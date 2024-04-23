5e78594cc00662e51941a5f239123d1d064cf0cc
Ana Gabriela <anagabrieladev@gmil.com>
2024-04-22 23:55:00 -0300

    feat: q1

--- src/q1_AnaBezerra.py
+++ src/q1_AnaBezerra.py
@@ -1,18 +1,20 @@
-create_transaction = lambda transaction_type: receive_cash() and transaction_type == "Cash" or fund_transfer() and transaction_type == "Fund Transfer" or request_credit_account_details and transaction_type == "Credit"
+create_transaction = lambda transaction_type, amount, accountConfirmation, user, password: \
+    receive_cash(amount) if transaction_type == "Cash" else \
+    fund_transfer(user, password, accountConfirmation) if transaction_type == "Fund Transfer" else \
+    request_credit_account_details(user, password, accountConfirmation) if transaction_type == "Credit" and accountConfirmation else \
+    None
 
-fund_transfer = lambda: fund_transfer_details(getAccount(), "asd")
+fund_transfer = lambda user, password, AccountAllowed: fund_transfer_details(user, password) or accountConfirmation(AccountAllowed)
+accountConfirmation = lambda AccountAllowed: [close_transaction() if AccountAllowed else cancel_transaction()] #abstrai a confirmacao de conta para que ja venha confirmado de outra camada do sistema (O que seria o controle chamaria uma funcao de confirmacao de conta e passaria o resultado)
+fund_transfer_details = lambda user, password: print({"user": user, "password": "*****"}) #somente para mostrar os dados de usuario
 
-fund_transfer_details = lambda account, password: print({"user": account, "password": password})
-getAccount = lambda: str(input("Account"))
-
-receive_cash = lambda: receive_cash_printer(receive_cash_input())
+receive_cash = lambda ammount: receive_cash_printer(ammount) or print_payment_receipt()
 receive_cash_printer = lambda value: print("Payment Receipt of ", value,'$')
-receive_cash_input = lambda: int(input("How much Cash? "))
 print_payment_receipt = lambda: print("Print Payment Receipt") or complete_transaction()
 complete_transaction = lambda: print("Transaction completed!")
 
-request_credit_account_details = lambda: print("Request Credit Account Details") or request_payment_from_bank()
-request_payment_from_bank = lambda: print("Request Payment from Bank") or provide_bank_deposit_details()
+request_credit_account_details = lambda user, password, accountConfirmation: print("Requesting Credit Account Details...") or request_payment_from_bank()
+request_payment_from_bank = lambda: print("Requesting Payment from Bank") or provide_bank_deposit_details()
 provide_bank_deposit_details = lambda: print("Provide Bank Deposit Details") or check_payment_confirmation(True)
 
 check_payment_confirmation = lambda bank_confirmation: [confirm_payment_approval_from_bank() if bank_confirmation else cancel_transaction()][0]
@@ -21,4 +23,12 @@ confirm_payment_approval_from_bank = lambda: print("Confirm Payment Approval fro
 cancel_transaction = lambda: print("Cancel Transaction")
 close_transaction = lambda: print("Close Transaction")
 
-q1 = lambda: create_transaction(input("Type 'Cash' or 'Credit' or 'Fund Transfer'\n"))
+print_create_transaction = lambda : print("Type 'Cash' or 'Credit' or 'Fund Transfer'\n")
+
+transaction = lambda type, amount, AccountAllowed, user, password: (print_create_transaction(), create_transaction(type, amount, AccountAllowed, user, password)) or print(type + " Selected")
+
+#se chamar pelo index, la os dados estao sendo enviados para q1
+q1 = lambda type, amount, AccountAllowed, user, password: transaction(type, amount, AccountAllowed, user, password) 
+
+#Nao implementei dados mocados para uma lista de usuario mas seria possivel utilizar o banco de dados para guardar as transacoes que o usuario fez, o score de credito e afins. 
+#Tentei utilizar os dados como parametros pois poderiam ser reaproveitados no exercicio 5.
